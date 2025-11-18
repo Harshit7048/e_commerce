85eae4b4a07d5c429032964063d34b13aabd5694
Harshit Bhardwaj <harshit704892@gmail.com>
2025-11-18 08:30:07 +0530

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Home from "./Pages/Home/Home"
 import Header from "./Components/Header/Header"
 import Footer from "./Components/Footer/Footer"
 import UserContextProvider from "./Components/Context/UserContextProvider"
-
+import { HashRouter } from "react-router-dom"
 
 import Login from './Pages/Login/Login.jsx'
- import Cart from './Pages/Cart.jsx'
- import Product from "./Pages/Product.jsx"
+import Cart from './Pages/Cart.jsx'
+import Product from "./Pages/Product.jsx"
 import ShopCategory from "./Pages/ShopCategory.jsx"
 import banner_mens from "./Components/Assets/banner_mens.png"
 import banner_women from './Components/Assets/banner_women.png'
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <UserContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Header />
 
         <Routes>
@@ -29,7 +29,7 @@ function App() {
           <Route path="/womens" element={<ShopCategory category="women" banner={banner_women} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/contact" element={<Contact/>} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
 
@@ -37,7 +37,9 @@ function App() {
         </Routes>
 
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
+
+
     </UserContextProvider>
   )
 }
