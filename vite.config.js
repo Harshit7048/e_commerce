9ab711424b8fdef33475d3ec8dccd68d0cd2264b
Harshit Bhardwaj <harshit704892@gmail.com>
2025-11-18 08:22:09 +0530

    added config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// export default defineConfig({
-//   plugins: [
-//     tailwindcss(),
-//   ],
 
-// })
-export default {
+export default defineConfig({
   base: "/e-commerce/",
-};
+  plugins: [
+    react(),
+    tailwindcss()
+  ]
+})
